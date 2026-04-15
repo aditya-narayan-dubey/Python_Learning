d6da9e6bba9acb3686f81b9f9a7a2a810dbe988a
Aditya Dubey <uhai3582@gmail.com>
2026-04-15 09:58:13 +0530

    Add files via upload

---
+++ 04_operators.py
@@ -0,0 +1,25 @@
+# Arithmetic operators
+
+a=7
+b=6
+c=a+b
+print (c)
+
+# Assignment operator
+
+a= 4-2 #assign 4-2 in a
+print(a)
+
+b=6
+b += 3 #increment the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison operator
+
+d= 5<4
+print(d)
+
+# Logical operators
+
+e = True or False
+print (e)
